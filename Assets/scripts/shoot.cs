cfc307955c69480dcfdb83e40523769000280cea
juliusg13 <juliusg13@ru.is>
2017-07-01 16:45:10 +0200

    the game, first push

---
+++ Assets/scripts/shoot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    GameObject ball;
+    Vector2 dir;
+    // Use this for initialization
+    void Start () {
+        ball = GameObject.FindGameObjectWithTag("ball");
+        dir = new Vector2(2, 0);
+        ball.GetComponent<Rigidbody2D>().AddForce(dir * 5, ForceMode2D.Impulse);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
